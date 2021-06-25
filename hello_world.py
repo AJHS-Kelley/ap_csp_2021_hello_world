6078b202da15b8f1e44a5f61c3c732c407ccce97
Ryan Kelley <kelleyr@duvalschools.org>
2021-06-24 22:06:56 -0400

    Create hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,12 @@
+# You will use this file to write your first Python program and make your first commits and submits to GitHub.
+# You are going to write a program that will print the words, "Hello, world!" onto the screen. 
+# To do this, you are going to use a special print() function built into Python. 
+# First, edit the comment on Line 5 below with your FIRST NAME and LAST INITIAL. Remove the < > characters on the line.
+# <First Name> <Last Initial>
+# Make your first commit and submit.  The teacher will demonstrate this process.  Please pay attention!  
+# At the start of Line 9 below, type print()
+# This line of code tells Python to print on the screen.  Now we need to tell it what to print.
+# Inside the () on Line 9, type "Hello, world!"  Make sure to include the quotation marks.  
+
+# Run your program.  Did your string print correctly on the screen? 
+# If yes, commit and submit your code 
